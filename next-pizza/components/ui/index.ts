84225537efe20b4d3d84d21dd752feaf52b73eb4
Ekaterina <ekaterina@MacBook-Pro-Ekaterina.local>
2024-09-06 19:20:26 +0300

    Initial commit

---
+++ next-pizza/components/ui/index.ts
@@ -0,0 +1,9 @@
+export { Button } from "./button";
+export { Checkbox } from "./checkbox";
+export { Dialog } from "./dialog";
+export { Input } from "./input";
+export { Select } from "./select";
+export { Skeleton } from "./skeleton";
+export { Slider } from "./slider";
+export { Popover } from "./popover";
+export { Drawer } from "./drawer";
